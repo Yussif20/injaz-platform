29be1bf14144024b2ac014fcb74fc41cad2e9b9f
yussif20 <yousefayman2000500@gmail.com>
2026-04-03 19:28:57 +0200

    safely remove any extra slash

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,5 +1,6 @@
-export const BACKEND_API_URL =
+const raw =
   process.env.NEXT_PUBLIC_BACKEND_API_URL || "https://staging.enjazfile.com";
+export const BACKEND_API_URL = raw.replace(/\/+$/, "");
 
 export const COOKIE_NAMES = {
   AUTH_TOKEN: "admin_auth_token",
